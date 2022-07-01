07bce11591dc9a204d93f2f9bd98c00f3d4aa300
Diego Duran <diegodurandaza6@gmail.com>
2022-07-01 06:00:41 -0500

    Configuración de parametros http para peticiones con parametros, configuración de efectos con Animate CSS

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -8,13 +10,31 @@ import { Component } from '@angular/core';
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
   regionActiva: string = '';
+  paises: Country[] = [];
+  termino: string = '';
+  hayError: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region:string): string{
+    return (region === this.regionActiva) ? 'btn btn-sm btn-primary me-2' : 'btn btn-sm btn-outline-primary me-2';
+  }
 
   activarRegion(region:string){
+    if (this.regionActiva === region) return;
     this.regionActiva = region;
     //TODO: hacer el llamado al servicio.
+    this.paises = [];
+    this.paisService.buscarRegion(region).subscribe({
+      next: (resp) => {
+        // console.log(resp);
+        this.paises = resp;
+      }, error: (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    });
   }
 }
