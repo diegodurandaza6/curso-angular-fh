85e08bdda0a6a9a793c0cdcd02f3c5edb034b816
Diego Duran <diegodurandaza6@gmail.com>
2022-07-08 08:13:14 -0500

    Configuración de pipes de seleccion entre opciones (i18nSelect) y selección de cantidades (i18nPlural)

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html'
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Diego';
+  genero: string = 'masculino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla'
   }
 
+  cambiarPersona(){
+        
+    if(this.genero == 'femenino'){
+      this.genero = 'masculino';
+      this.nombre = 'Diego';
+    }else{
+      this.genero = 'femenino';
+      this.nombre = 'Camila';
+    }
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Diego', 'Camila'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  borrarCliente(){
+    if(this.clientes.length === 0){
+      this.clientes = ['Maria', 'Pedro', 'Juan', 'Diego', 'Camila']
+    }
+    this.clientes.pop();
+  }
 }
