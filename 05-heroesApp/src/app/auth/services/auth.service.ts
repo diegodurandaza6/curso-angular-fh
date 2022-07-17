afdf12b34bec7c63cfc6c38dab320208c86ef2af
Diego Duran <diegodurandaza6@gmail.com>
2022-07-17 08:00:56 -0500

    Protección de rutas, login basico, servicio de autenticación, CanLoad y CanActivate en Angular Guards, localStorage para almacenar sesión

---
+++ 05-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../interfaces/auth.interace';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth():Auth {
+    return { ...this._auth! };
+  }
+
+  constructor(private http: HttpClient) { }
+
+  verificarAutenticación(): Observable<boolean>{
+    if(!localStorage.getItem('token')){
+      return of(false);
+    }
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+    .pipe(
+      map(auth => {
+        this._auth = auth;
+        return true;
+      })
+    )
+  }
+
+  login(){
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+    .pipe(
+      tap(resp => this._auth = resp),
+      tap(auth => localStorage.setItem('token', auth.id))
+    )
+  }
+
+  logout() {
+    this._auth = undefined;
+  }
+}
