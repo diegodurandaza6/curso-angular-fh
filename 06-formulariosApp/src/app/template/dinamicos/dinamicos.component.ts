23a483ac77a41d1131d8642f9866ceb98bd3af31
Diego Duran <diegodurandaza6@gmail.com>
2022-07-19 05:18:02 -0500

    Formularios por template dinamicos y switches

--- 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild('miForm') miForm!: NgForm
 
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Diego',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Otra cosa' }
+    ]
+  }
+
+  nombreValido(): boolean {
+    return this.miForm?.controls['nombre']?.invalid && this.miForm?.controls['nombre']?.touched;
+  }
+
+  guardar() {
+    return console.log('Formulario posteado');
+  }
+
+  eliminar(idx: number): void {
+    this.persona.favoritos.splice(idx, 1);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito:Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
   }
 
 }
