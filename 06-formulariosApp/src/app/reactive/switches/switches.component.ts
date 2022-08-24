c8c8209980506de55c9c609a3697b8992be9ac23
Diego Duran <diego.duran@pragma.com.co>
2022-08-24 05:56:55 -0500

    Configuración de switches reactivos (dinamicos)

--- 06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ 06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit{
 
   miFormulario: FormGroup = this.fb.group({
     genero:['M', Validators.required],
-    notificaciones:['M', Validators.required]
+    notificaciones:['M', Validators.required],
+    condiciones: [false, Validators.requiredTrue]
   });
 
   persona = {
@@ -19,4 +20,21 @@ export class SwitchesComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.miFormulario.reset({...this.persona, condiciones:false});
+
+    // this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValude => {
+    //   console.log(newValude);
+    // })
+
+    this.miFormulario.valueChanges.subscribe(({condiciones, ...restoArg}) => {
+      this.persona = restoArg;
+    });
+  }
+
+  guardar(){
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+    this.persona = formValue;
+  }
 }
